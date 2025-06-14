89eaacb7864e8682fa881873f1c452357c1ee829
Kawsar Uddin Ahmed Chowdhury <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2025-06-14 11:52:59 +0000

    Update Consecutive_Digits_To_Form_Sum.py

--- Consecutive_Digits_To_Form_Sum.py
+++ Consecutive_Digits_To_Form_Sum.py
@@ -14,8 +14,8 @@ def consecutive_ducks(n):
                break
     #print(result)
     #a = sorted(result)
-    #b = list(range(min(result),max(result)+1)) #Here I will create list that will be sorted from minimun to max number
-    if (result): # It is finding the array elements are sorted or not.
+    #b = list(range(min(result), max(result)+1)) #Here I will create a list that will be sorted from minimum to maximum number
+    if (result): # It is finding whether the array elements are sorted or not.
         return True
     else:
         return False
@@ -28,11 +28,11 @@ print(v)
          #******************************** Second Solution ***********************************#
          
 def consecutive_ducks(n):
-    return (n & (n - 1)) != 0 #Here I have used bit techonlogy and gate techonlogy of computer. Here I have
-                              #used AND gate between n , n-1. Here , the n and n-1 number will be changed
-                              # into 0 , 1 bit and then perfom the AND operation.
-                              #Now If the result of the AND operation is zero  then the n is power of 2 and
-                              #non consecutive ducks.(means adding consecutive number you can't get the number)
+    return (n & (n - 1)) != 0 # Here, I have utilized bit and gate technology from computer science. Here I have
+                              #used an AND gate operation between n, n-1. Here, the n and n-1 numbers will be changed
+                              # into 0, 1 bit, and then perform the AND operation.
+                              #Now, if the result of the AND operation is zero,  then n is a power of 2 and
+                              # non-consecutive ducks. (means adding consecutive numbers, you can't get the number)
 
 v = consecutive_ducks(755)
 print(v)
